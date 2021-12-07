3c40564504e03203ffd6f2494b26c8d42caf250b
Amit Kumar Sanghi <94451087+Amit-Sanghi@users.noreply.github.com>
2021-12-07 19:46:55 +0530

    done

--- app.js
+++ app.js
@@ -4,10 +4,26 @@ var outputtxt = document.querySelector("#output")
 
 var url ="https://api.funtranslations.com/translate/groot.json"
 
-function()
+function serverURL(text) {
+        return url + "?" + "text=" + text
+}
+
+function errorHandler(){
+    console.log("error occured", error)
+    alert("something went wrong")
+}
 
 function clickHandler(){
     console.log("clicked!")
+    var text = inputtxt.value
+    
+    fetch(serverURL(text))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated
+        outputtxt.innerText = translatedText
+    })
+
 }
 
 document.getElementById("translate").addEventListener("click", clickHandler)
