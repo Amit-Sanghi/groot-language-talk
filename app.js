d89949fe0f9fa00fde644857de2199d4d87b13da
Amit Kumar Sanghi <94451087+Amit-Sanghi@users.noreply.github.com>
2021-12-07 10:25:36 +0530

    update: javascript

---
+++ app.js
@@ -0,0 +1,13 @@
+var btnTranslate = document.querySelector("#translate")
+var inputtxt = document.querySelector("#txt-input")
+var outputtxt = document.querySelector("#output")
+
+var url ="https://api.funtranslations.com/translate/groot.json"
+
+function()
+
+function clickHandler(){
+    console.log("clicked!")
+}
+
+document.getElementById("translate").addEventListener("click", clickHandler)
